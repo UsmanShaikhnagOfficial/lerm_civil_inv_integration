606f9d8c93702bf97c61551a863433dab1936e30
UsmanShaikhnagOfficial <usman.shaikhnag@esehat.org>
2023-06-23 15:53:48 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{'name':'LERM_CIVIL_INV_Integration',
+ 'summary': "LERM_CIVIL_INV_INTEGRATION",
+ 'author': "Usman Shaikhnag", 
+ 'website': "http://www.esehat.org", 
+ 'category': 'Uncategorized', 
+ 'version': '13.0.1', 
+ 'depends':['base' , 'contacts','lerm_civil'],
+ 'data': [
+    'views/menu.xml',
+    'views/sample_pending_invoice.xml',
+    ]
+}
